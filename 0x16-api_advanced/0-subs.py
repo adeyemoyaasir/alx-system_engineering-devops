ba7cc93d46277ff0302aeacd8e3181dbbb4daaca
Adeyemo Yaasir <adeyemoyaasir@gmail.com>
2024-08-13 12:34:14 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 202:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
