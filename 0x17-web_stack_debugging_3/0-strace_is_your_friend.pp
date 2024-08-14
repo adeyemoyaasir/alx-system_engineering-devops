afb0d41d8710d35804ee8e1c5783bc1cd69973a8
Adeyemo Yaasir <adeyemoyaasir@gmail.com>
2024-08-14 10:26:33 -0700

    webstack

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
+# Fixing Apache returning a 500 error
 
-exec {'replace':
-  provider => shell,
+exec { 'fix error':
+  provider => 'shell',
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-} 
+}
+
